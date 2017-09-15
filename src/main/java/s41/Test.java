c2399741c6ac9d72dc96b296031bc44533ae9c73
Zz-m <adjim234@gmail.com>
2017-09-15 14:43:54 +0800

    修复maven依赖

--- src/main/java/s41/Test.java
+++ src/main/java/s41/Test.java
@@ -1,12 +1,16 @@
 package s41;
 
+import edu.princeton.cs.algorithms.Graph;
+
 /**
  * t1
  * Created by dhx on 2017/2/8.
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("ok");
-
+        System.out.println("start...");
+        edu.princeton.cs.algorithms.Graph graph = new Graph(3);
+        graph.addEdge(1,2);
+        System.out.println(graph);
     }
 }
