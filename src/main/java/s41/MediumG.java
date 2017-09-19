cbdc97d7874e1f8f6da512caf1e2a903f895ce50
zz-m <adjim234@gmail.com>
2017-09-19 16:19:41 +0800

    修改图输入文件方式

--- src/main/java/s41/MediumG.java
+++ src/main/java/s41/MediumG.java
@@ -3,15 +3,21 @@ package s41;
 import edu.princeton.cs.introcs.In;
 import edu.princeton.cs.introcs.StdOut;
 
+import java.io.File;
+
 public class MediumG {
     private MediumG() {
     }
 
     public static Graph getMediumG() {
-        return new Graph(new In("F:\\MediumG.txt"));
+        File currentDirFile = new File(".");
+        return new Graph(new In(currentDirFile.getAbsolutePath() + "/res/mediumG.txt"));
     }
 
     public static void main(String[] args) {
+        File currentDirFile = new File(".");
+        StdOut.println(currentDirFile.getAbsolutePath() + "/res/mediumG.txt");
+
         StdOut.println(getMediumG().E());
     }
 }
