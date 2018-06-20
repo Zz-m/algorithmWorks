43aaefe680937e32d12147d53d893cae8041575b
zz-m <adjim234@gmail.com>
2018-06-20 11:31:34 +0800

    KruskalMST

---
+++ src/main/java/s43/KruskalMST.java
@@ -0,0 +1,42 @@
+package s43;
+
+import edu.princeton.cs.algorithms.MinPQ;
+import edu.princeton.cs.algorithms.Queue;
+import edu.princeton.cs.algorithms.UF;
+
+/**
+ * Created by dhx on 2018/6/20.
+ */
+public class KruskalMST implements MST {
+
+    private Queue<Edge> mst = new Queue<>();
+
+    public KruskalMST(EdgeWeightedGraph edgeWeightedGraph) {
+        MinPQ<Edge> pq = new MinPQ<>();
+        for (Edge edge : edgeWeightedGraph.edges())
+            pq.insert(edge);
+
+        UF uf = new UF(edgeWeightedGraph.v());
+        while (!pq.isEmpty() && mst.size() < edgeWeightedGraph.v() - 1) {
+            Edge e = pq.delMin();
+            int v = e.either(), w = e.other(v);
+            if (!uf.connected(v, w)) {
+                uf.union(v, w);
+                mst.enqueue(e);
+            }
+        }
+    }
+
+    @Override
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+
+    @Override
+    public double weight() {
+        double weight = 0.0;
+        for (Edge edge : mst)
+            weight += edge.weight();
+        return weight;
+    }
+}
--- src/main/java/s43/MST.java
+++ src/main/java/s43/MST.java
@@ -1,32 +1,22 @@
 package s43;
 
-import edu.princeton.cs.introcs.In;
-import edu.princeton.cs.introcs.StdOut;
-
 /**
  * Created by dhx on 2018/5/11.
  * minimum spanning tree   最小生成树
  */
-public class MST {
+public interface MST {
 
-    public MST(EdgeWeightedGraph edgeWeightedGraph) {}
+    Iterable<Edge> edges();
 
-    public Iterable<Edge> edges() {
-        return null;
-    }
+    double weight();
 
-    public double weight() {
-        return 0;
-    }
-
-
-    public static void main(String[] args) {
-        In in = new In("tinyEWG.txt");
-        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
-
-        MST mst = new MST(G);
-        for (Edge e : mst.edges())
-            StdOut.println(e);
-        StdOut.println(mst.weight());
-    }
+//    public static void main(String[] args) {
+//        In in = new In("tinyEWG.txt");
+//        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+//
+//        MST mst = new MST(G);
+//        for (Edge e : mst.edges())
+//            StdOut.println(e);
+//        StdOut.println("weight: " + mst.weight());
+//    }
 }
