58bae3243a36715890d1595c0bc42c6f682dd529
Zz-m <adjim234@gmail.com>
2021-02-26 14:35:12 +0800

    code format

---
+++ src/main/java/cn/denghanxi/leetcode/q6/Solution.java
@@ -0,0 +1,11 @@
+package cn.denghanxi.leetcode.q6;
+
+/**
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ */
+class Solution {
+    public String convert(String s, int numRows) {
+
+        return null;
+    }
+}
