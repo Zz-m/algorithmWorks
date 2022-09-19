8f63e618d123314262394f671c4dbc145548e45d
dhx <adjim234@gmail.com>
2022-09-19 11:18:30 +0800

    q9 2

--- src/main/java/cn/denghanxi/leetcode/q9/Solution.java
+++ src/main/java/cn/denghanxi/leetcode/q9/Solution.java
@@ -1,10 +1,22 @@
 package cn.denghanxi.leetcode.q9;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Solution {
     public boolean isPalindrome(int x) {
-        String s = Integer.toString(x);
-        StringBuilder sb1 = new StringBuilder(s).reverse();
-        return s.equals(sb1.toString());
+        if (x < 0) return false;
+        List<Integer> list = new ArrayList<>();
+        while (x != 0) {
+            list.add(x % 10);
+            x = x / 10;
+        }
+        for (int i = 0; i < list.size() / 2; i++) {
+            if (!Objects.equals(list.get(i), list.get(list.size() - 1 - i)))
+                return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
